f5896e7396c9bddae792b544b0b59f161ca4268a
skill-Tzy <edu1blogger@gmail.com>
2024-02-19 06:20:36 +0000

    Update file Yaml

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # Membuat Docker image dari Dockerfile dengan nama item-app dan tag v1
-docker build docker build -t item-app:v1 .
+docker build -t item-app:v1 .
 
 # melihat daftar image di lokal
 docker images
 
 # mengubah nama image agar sesuai dengan format GitHub Container Registry
-docker tag item-app ghcr.io/adzkiaadi/item-app:v1
+docker tag item-app:v1 ghcr.io/skill-tzy/item-app:v1
 
 # login masuk ke GitHub Container Registry
-echo $PAT | docker login ghcr.io --username adzkiaadi --password-stdin
+docker login --username skill-tzy --password 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 ghcr.io
 
 # mengunggah image ke GitHub Container Registry
-docker push ghcr.io/adzkiaadi/item-app:v1
+docker push ghcr.io/skill-tzy/item-app:v1
--- test.sh
+++ test.sh
@@ -6,24 +6,17 @@ image_version="v1"
 github_account="skill-tzy"
 
 # membuat image di Docker
-echo -e "Building a new Docker image:"
 docker build . -t $image_name:$image_version
 
 # melihat daftar image di lokal
-echo -e "\nViewing the list of Docker images:"
 docker images
 
 # mengubah nama image agar sesuai dengan format GitHub Container Registry
-echo -e "\nGiving a new tag name to the Docker image to match the format of GitHub Container Registry:"
 docker tag $image_name:$image_version ghcr.io/$github_account/$image_name:$image_version
 docker images
 
 # log masuk ke GitHub Container Registry
-echo -e "\nLogin into GitHub Container Registry:"
 docker login docker.pkg.github.com -u $github_account
 
 # mengunggah image ke GitHub Container Registry
-echo -e "\nPublishing image to GitHub Container Registry:"
 docker push ghcr.io/$github_account/$image_name:$image_version
-
-echo -e "\nDone."
