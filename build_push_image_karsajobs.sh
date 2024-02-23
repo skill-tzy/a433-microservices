60886cd8930a5f906e24850e27868f3425a6df74
root <edu1blogger@gmail.com>
2024-02-23 03:57:37 +0000

    Update

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -7,7 +7,7 @@ docker build -t skill-tzy/karsajobs:latest .
 docker tag skill-tzy/karsajobs:latest ghcr.io/skill-tzy/karsajobs:latest
 
 # login masuk ke GitHub Container Registry
-docker login --username skill-tzy --password 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 ghcr.io
+docker login --username skill-tzy --password $GHCR_PAT_TOKEN ghcr.io
 
 # mengunggah image ke GitHub Container Registry
 docker push ghcr.io/skill-tzy/karsajobs:latest
